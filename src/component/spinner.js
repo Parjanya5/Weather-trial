27cfec8a033a95569adc8f8eba6da2ac47f01434
Parjanya5 <168263089+Parjanya5@users.noreply.github.com>
2026-04-07 22:58:33 +0530

    fixed img alt error

--- src/component/spinner.js
+++ src/component/spinner.js
@@ -4,7 +4,7 @@ import Load from '../load.gif'
 export default function spinner() {
   return (
     <div>
-      <img src={Load} alt='forget'></img>
+      <img src={Load} alt="loading..."></img>
     </div>
   )
 }
